6e21966c0f6e9073643919430c54fc60c0fc8636
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-06-09 19:36:12 -0300

    Create grafo.h

---
+++ include/grafo.h
@@ -0,0 +1,44 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+#include <stddef.h>
+
+/*
+   Um grafo é um tipo de 
+*/
+
+typedef void* Grafo;
+
+Grafo criaGrafo();
+/*
+Fazer dps
+*/
+
+void adicionarVertice();
+/*
+Fazer dps
+*/
+
+void adicionarAresta();
+/*
+Fazer dps
+*/
+
+void offAresta();
+/*
+Fazer dps
+*/
+
+void onAresta();
+/*
+Fazer dps
+*/
+
+
+
+Grafo deletaGrafo();
+/*
+Fazer dps
+*/
+
+#endif
